0ef38fb89186a4e21a900e08a11a743124fe7092
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-06-30 00:30:36 +0300

    Two more methods

--- Basic_Algorithms/Algo3_Palindromes.js
+++ Basic_Algorithms/Algo3_Palindromes.js
@@ -1,6 +1,5 @@
 function palindrome(str) {
-    str = str.toLowerCase();
-    str = str.replace(/\W\d|\_/gi, ' ');
+    str = str.toLowerCase().replace(/\W\d|\_/gi, ' ');
     var re = /[\w+]/gi;
     var strUpdated = str.match(re);
     var strUpJoin = strUpdated.join('');
@@ -10,3 +9,110 @@ function palindrome(str) {
 
 
 palindrome("eye");
+
+
+// Two more ways to check for a polindromes in JavaScript
+
+// to pass the last test aka palindrome(“0_0 (: /-\ :) 0–0”) you should use the following RE
+
+/*/[^A-Za-z0-9]/g;
+/[\W_]/g;
+*/
+// I. Check for Palindromes with Built-In Functions
+
+// Using
+// 1. toLowerCase();
+// 2. replace();
+// 3. split();
+// 4. reverse();
+// 5. join();
+
+function palindrome(str) {
+    // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+    var re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
+
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    // str.toLowerCase() = "A man, a plan, a canal. Panama".toLowerCase() = "a man, a plan, a canal. panama"
+    // str.replace(/[\W_]/g, '') = "a man, a plan, a canal. panama".replace(/[\W_]/g, '') = "amanaplanacanalpanama"
+    // var lowRegStr = "amanaplanacanalpanama";
+
+
+    // Step 2. Use the same chaining methods with built-in functions from the previous article 'Three ways to Reverse a String in JavaScript
+    var reverseStr = lowRegStr.split('').reverse().join('');
+
+    // lowRegStr.split('') = "amanaplanacanalpanama".split('') = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+    // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].reverse() = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+    // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].join('') = "amanaplanacanalpanama"
+    // So, "amanaplanacanalpanama".split('').reverse().join('') = "amanaplanacanalpanama";
+    // And, var reverseStr = "amanaplanacanalpanama";
+
+    // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
+    return reverseStr === lowRegStr;
+    // "amanaplanacanalpanama" === "amanaplanacanalpanama"? => true
+
+}
+palindrome("A man, a plan, a canal. Panama");
+
+
+function palindrome(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.lowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join('');
+    var reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
+
+
+// II. Check for Plaindromes With a FOR loop
+
+function palindrome(str) {
+    // Step 1. The first part is the same as earlier
+    var re = /[^A-Za-z0-9]/g; // or var re = /[\W_]/g;
+    str = str.toLowerCase().replace(re, '');
+
+    // Step 2. Create the FOR loop
+    var len = str.length; // var len = "A man, a plan, a canal. Panama".length = 30
+
+    for (var i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) { // As long as the characters from each part match, the FOR loop will go on
+            return false; // When the characters don't match anymore, false is returned and we exit the FOR loop
+        }
+        /* Here len/2 = 15
+           For each iteration: i = ?    i < len/2    i++    if(str[i] !== str[len - 1 - i])?
+           1st iteration:        0        yes         1     if(str[0] !== str[15 - 1 - 0])? => if("a"  !==  "a")? // false
+           2nd iteration:        1        yes         2     if(str[1] !== str[15 - 1 - 1])? => if("m"  !==  "m")? // false      
+           3rd iteration:        2        yes         3     if(str[2] !== str[15 - 1 - 2])? => if("a"  !==  "a")? // false  
+           4th iteration:        3        yes         4     if(str[3] !== str[15 - 1 - 3])? => if("n"  !==  "n")? // false  
+           5th iteration:        4        yes         5     if(str[4] !== str[15 - 1 - 4])? => if("a"  !==  "a")? // false
+           6th iteration:        5        yes         6     if(str[5] !== str[15 - 1 - 5])? => if("p"  !==  "p")? // false
+           7th iteration:        6        yes         7     if(str[6] !== str[15 - 1 - 6])? => if("l"  !==  "l")? // false
+           8th iteration:        7        yes         8     if(str[7] !== str[15 - 1 - 7])? => if("a"  !==  "a")? // false
+           9th iteration:        8        yes         9     if(str[8] !== str[15 - 1 - 8])? => if("n"  !==  "n")? // false
+          10th iteration:        9        yes        10     if(str[9] !== str[15 - 1 - 9])? => if("a"  !==  "a")? // false
+          11th iteration:       10        yes        11    if(str[10] !== str[15 - 1 - 10])? => if("c" !==  "c")? // false
+          12th iteration:       11        yes        12    if(str[11] !== str[15 - 1 - 11])? => if("a" !==  "a")? // false
+          13th iteration:       12        yes        13    if(str[12] !== str[15 - 1 - 12])? => if("n" !==  "n")? // false
+          14th iteration:       13        yes        14    if(str[13] !== str[15 - 1 - 13])? => if("a" !==  "a")? // false
+          15th iteration:       14        yes        15    if(str[14] !== str[15 - 1 - 14])? => if("l" !==  "l")? // false
+          16th iteration:       15        no               
+         End of the FOR Loop*/
+    }
+    return true; // Both parts are strictly equal, it returns true => The string is a palindrome
+}
+
+palindrome("A man, a plan, a canal. Panama");
+
+
+function palindrome(str) {
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+palindrome("A man, a plan, a canal. Panama");
