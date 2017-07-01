27b83c846c41f67c27fc9049f4dd5bf2fcf2e278
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-01 20:11:29 +0300

    Algo10_solved_truncate

---
+++ Basic_Algorithms/Algo10_Truncate.js
@@ -0,0 +1,45 @@
+// Truncate a string
+/*
+=================================
+        My Answer
+=================================
+*/
+
+
+function truncateString(str, num) {
+    // Clear out that junk in your trunk
+
+    var end = '...';
+
+    if (str.length < 3) {
+        return str.slice(0, num).concat(end);
+    } else if (num === 2) {
+        return str.slice(0, num).concat(end);
+    } else if (num > 1 && num < 3) {
+        return str.slice(0, num - 3).concat(end);
+    } else if (num >= str.length) {
+        return str.slice(0, num);
+    } else {
+        return str.slice(0, num - 3).concat(end);
+    }
+
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+
+/*
+=================================
+        testing!
+=================================
+*/
+
+
+var str = "A-tisket a-tasket A green and yellow basket",
+    num = 11,
+    end = '...';
+
+answer = "A-tisket..."
+
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
