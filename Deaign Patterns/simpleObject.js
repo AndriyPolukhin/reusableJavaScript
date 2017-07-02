ca983022c9e103782fbfa175ac69be4cc7a1e720
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-02 23:21:36 +0300

    Simple Objects Creation

---
+++ Deaign Patterns/simpleObject.js
@@ -0,0 +1,62 @@
+// A car "class"
+
+function Car(model) {
+
+    this.model = model;
+    this.color = 'silver';
+    this.year = '2012';
+
+    this.getInfo = function () {
+        return this.model + ' ' + this.year;
+    };
+}
+
+var myCar = new Car('ford');
+myCar.year = '2010';
+console.log(myCar.getInfo());
+
+
+// Three ways to difine a class in JavaScript
+
+// 1. sing a function
+
+function Apple(type) {
+    this.type = type;
+    this.color = 'red';
+    this.getInfo = getAppleInfo;
+}
+
+// anti-patter! 
+function getAppleInfo() {
+    return this.color + ' ' + this.type + ' apple';
+}
+
+var apple = new Apple('macintosh');
+apple.color = 'reddish';
+aler(apple.getInfo());
+
+
+// 1.1. Methods defined internally
+
+function Apple(type) {
+    this.type = type;
+    this.color = 'red';
+    this.getInfo = function () {
+        return this.color + ' ' + this.type + ' apple';
+    };
+}
+
+// 1.2. Methods added to the prototype
+
+function Apple(type) {
+    this.type = type;
+    this.color = 'red';
+}
+Apple.prototype.getInfo = function () {
+    return this.color + " " + this.type + ' apple';
+
+};
+
+
+
+// 2.
