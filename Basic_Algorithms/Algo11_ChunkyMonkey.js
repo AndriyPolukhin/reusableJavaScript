0dd58df2af4ae613994a47500627be26eb9ff2e5
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-02 23:21:57 +0300

    Algo11_testing

---
+++ Basic_Algorithms/Algo11_ChunkyMonkey.js
@@ -0,0 +1,41 @@
+// Chunky Monkey
+
+/*
+=================================
+        My Answer
+=================================
+*/
+
+
+function chunkArrayInGroups(arr, size) {
+    // Break it up.
+    return arr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
+
+
+/*
+=================================
+        testing!
+=================================
+*/
+
+
+function chunkArrayInGroups(arr, size) {
+    // Break it up.
+    return arr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
+
+ans = [["a", "b"], ["c", "d"]];
+
+var arr = ['a', 'b', 'c', 'd'],
+    size = 2,
+    ans = [];
