aec954422db7f9e7f9962d8f17d733bff217e43b
AndriyPolukhin <andriy.polukhin@gmail.com>
2017-07-03 17:58:57 +0300

    added Basic Constructor

--- Design Patterns/2012_JDP/JSD_CH9.js
+++ Design Patterns/2012_JDP/JSD_CH9.js
@@ -62,4 +62,25 @@ var driver = Object.create(man);
 defineProp(driver, 'topSpeed', '100mph');
 driver.topSpeed
 
+/*
+=========================
+THe Constructor Pattern
+=========================
+*/
 
+// Basic constructor
+
+function Car(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+    
+    this.toString = function () {
+        return this.model + ' has done ' + this.miles + ' miles';
+    };
+}
+
+var civic = new Car('Honda Civic', 2009, 20000);
+var mondeo = new Car('Ford Mondeo', 2010, 5000);
+console.log(civic.toString());
+console.log(mondeo.toString());
