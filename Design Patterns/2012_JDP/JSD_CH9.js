f3da0605c1aca9ad5d41edba6e707e102fd6a211
AndriyPolukhin <andriy.polukhin@gmail.com>
2017-07-04 11:20:57 +0300

    Added Constructor With Prototypes

--- Design Patterns/2012_JDP/JSD_CH9.js
+++ Design Patterns/2012_JDP/JSD_CH9.js
@@ -84,3 +84,21 @@ var civic = new Car('Honda Civic', 2009, 20000);
 var mondeo = new Car('Ford Mondeo', 2010, 5000);
 console.log(civic.toString());
 console.log(mondeo.toString());
+
+
+
+// Constructors With Prototypes
+
+function Car(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+}
+
+Car.prototype.toString = function () {
+    return this.model +' has doen ' + this.miles + ' miles';
+};
+
+var civic = new Car('Honda Civic', 2009, 20000);
+var mondeo = new Car('Ford Mondeo', 2010, 5000);
+consle.log(civic.toString);
