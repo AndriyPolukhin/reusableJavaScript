f447da4a9701a8e3013b9ad9ab0c4d6b1aeb9a27
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-08 12:28:18 +0300

    How to check a today

--- basic exercise/More_basics.js
+++ basic exercise/More_basics.js
@@ -6,3 +6,29 @@ var side3 = 7;
 var perimeter = (side1 + side2 + side3) / 2;
 var area = Math.sqrt(perimeter * ((perimeter - side1) * (perimeter - side2) * (perimeter - side3)));
 console.log(area);
+
+
+// How to get a current date
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+
+id(dd < 10) {
+    dd = '0' + dd;
+}
+
+if (mm < 10) {
+    mm = '0' + mm;
+}
+
+today = mm + '-' + dd + '-' + yyyy;
+console.log(today);
+today = mm '/' + dd + '/' + yyyy;
+console.log(today);
+today = dd + '/' + mm + '/' + yyyy;
+console.log(today);
+today = dd + '/' + mm + '/' + yyyy;
+console.log(today);
