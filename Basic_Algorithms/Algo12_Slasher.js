2c62187a07e14a9da8ea6dd46cec57b7c7b983df
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-12 00:36:11 +0300

    Algo_12 Solved

---
+++ Basic_Algorithms/Algo12_Slasher.js
@@ -0,0 +1,28 @@
+// Slasher Flick
+
+/*
+=================================
+        My Answer
+=================================
+*/
+
+function slasher(arr, howMany) {
+    // it doesn't always pay to be first
+
+    arr.splice(0, howMany);
+
+
+    return arr;
+}
+
+
+slasher([1, 2, 3], 2);
+
+
+// Return the remaining elements of an array after chopping n elements
+
+/*
+=================================
+        testing!
+=================================
+*/
