02b6e062977cbdcb20ee03df06f29b55f2f6fbac
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-12 00:42:18 +0300

    algo_13 Mutations Init

---
+++ Basic_Algorithms/Algo13_Mutations.js
@@ -0,0 +1,24 @@
+// Mutations
+
+/*
+=================================
+        My Answer
+=================================
+*/
+
+
+function mutation(arr) {
+    return arr;
+}
+
+mutation(["hello", "hey"]);
+
+
+
+// Return the remaining elements of an array after chopping n elements
+
+/*
+=================================
+        testing!
+=================================
+*/
