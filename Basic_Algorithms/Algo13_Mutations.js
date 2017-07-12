3cb5035034c90b6230791b4103cb88ee73aa2c45
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-12 08:00:27 +0300

    One more to go!

--- Basic_Algorithms/Algo13_Mutations.js
+++ Basic_Algorithms/Algo13_Mutations.js
@@ -22,3 +22,22 @@ mutation(["hello", "hey"]);
         testing!
 =================================
 */
+
+function mutation(arr) {
+    var arr1 = arr[0].toLowerCase();
+    var arr2 = arr[1].toLowerCase().split('');
+    var arrCheck;
+    for (var i = 0; i < arr2.length; i++) {
+        arrCheck = arr1.indexOf(arr2[i]);
+        console.log(arrCheck);
+        if (arrCheck === -1) {
+            return false;
+        } else if (arrCheck !== -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+mutation(["hello", "hey"]);
