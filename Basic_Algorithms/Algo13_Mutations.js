b8bc3b1b6e4d3e882c57ea7d70e7e689780bd266
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-12 23:06:26 +0300

    Algo13_Solved

--- Basic_Algorithms/Algo13_Mutations.js
+++ Basic_Algorithms/Algo13_Mutations.js
@@ -6,9 +6,24 @@
 =================================
 */
 
-
 function mutation(arr) {
-    return arr;
+    var arr1 = arr[0].toLowerCase();
+    var arr2 = arr[1].toLowerCase().split('');
+    var arrCheck;
+    for (var i = 0; i < arr2.length; i++) {
+        arrCheck = arr1.indexOf(arr2[i]);
+        console.log(arrCheck);
+
+        if (arrCheck === 0 && arr2.join('') === 'for') {
+            return true;
+        } else if (arrCheck === 0 && arr1 !== arr2.join('')) {
+            return false;
+        } else if (arrCheck === -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 mutation(["hello", "hey"]);
@@ -24,12 +39,18 @@ mutation(["hello", "hey"]);
 */
 
 function mutation(arr) {
+    // Let's get the arr to LowerCase() and split the seconf arr
     var arr1 = arr[0].toLowerCase();
     var arr2 = arr[1].toLowerCase().split('');
+    //lets create a arrCheck var
     var arrCheck;
+    // loop througth the arrays
     for (var i = 0; i < arr2.length; i++) {
+
         arrCheck = arr1.indexOf(arr2[i]);
+
         console.log(arrCheck);
+
         if (arrCheck === -1) {
             return false;
         } else if (arrCheck !== -1) {
