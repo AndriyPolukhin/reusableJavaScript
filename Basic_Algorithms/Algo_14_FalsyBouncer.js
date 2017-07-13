65c4c581bf28fb2b8e120c8bff5c7e37b1f4ef23
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-13 07:53:16 +0300

    created

---
+++ Basic_Algorithms/Algo_14_FalsyBouncer.js
@@ -0,0 +1,21 @@
+// Mutations
+
+/*
+=================================
+        My Answer
+=================================
+*/
+
+function bouncer(arr) {
+    // Don't show a false ID to this bouncer.
+    return arr;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+
+/*
+=================================
+        testing!
+=================================
+*/
