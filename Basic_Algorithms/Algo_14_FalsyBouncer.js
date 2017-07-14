1fe126bc7e55c234aedbfa5e9992cccd4146582a
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-14 07:53:15 +0300

    progress 5%

--- Basic_Algorithms/Algo_14_FalsyBouncer.js
+++ Basic_Algorithms/Algo_14_FalsyBouncer.js
@@ -17,6 +17,7 @@ bouncer([false, null, 0, NaN, undefined, ""])
 /*
 =================================
         testing!
+
 Remove all falsy values from an array.
 
 Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
@@ -31,3 +32,28 @@ Array.prototype.filter()
 
 =================================
 */
+
+
+
+
+
+// Searching in the Array
+
+
+var fruits = ['apple', 'banana', 'grapes', 'mango', 'oranges'];
+
+function filterItems(query) {
+    return fruits.filter(function (el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    })
+}
+console.log(filterItems('ap'));
+console.log(filterItems('an'));
+
+
+function isBigEnough(value) {
+    return value >= 10;
+}
+
+var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+// filtered is [12, 130, 44]
