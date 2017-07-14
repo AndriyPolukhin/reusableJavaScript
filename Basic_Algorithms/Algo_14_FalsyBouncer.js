f42db762866f7c0fc5e39741b8e8854d77efdcbe
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-14 06:47:55 +0300

    description add

--- Basic_Algorithms/Algo_14_FalsyBouncer.js
+++ Basic_Algorithms/Algo_14_FalsyBouncer.js
@@ -12,10 +12,22 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
-
+bouncer([false, null, 0, NaN, undefined, ""])
 
 /*
 =================================
         testing!
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Boolean Objects
+Array.prototype.filter()
+
+
 =================================
 */
