6bdf6f668a8606904d8ea940bbaeff78db36650c
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-15 18:15:29 +0300

    coders solution added

--- Basic_Algorithms/Algo11_ChunkyMonkey.js
+++ Basic_Algorithms/Algo11_ChunkyMonkey.js
@@ -169,3 +169,16 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
+
+
+// Solution 6
+
+function chunkArrayInGroups(arr, size) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i += size) {
+        var barr = arr.slice(i, i + size);
+        newArr.push(barr);
+    }
+    return newArr;
+}
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
