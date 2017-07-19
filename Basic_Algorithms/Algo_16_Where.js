dbcc77a0e995a45111c1c788c906439fa9a1b927
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-19 23:03:54 +0300

    Algo_16 Init

---
+++ Basic_Algorithms/Algo_16_Where.js
@@ -0,0 +1,23 @@
+// Seek & Destroy
+
+/*
+=================================
+        My Answer
+=================================
+*/
+
+function getIndexToIns(arr, num) {
+    // Find my place in this sorted array.
+    return num;
+}
+
+getIndexToIns([40, 60], 50);
+
+console.log('Should return 3: ' + getIndexToIns([10, 20, 30, 40, 50], 35));
+console.log('Should return 2: ' + getIndexToIns([10, 20, 30, 40, 50], 30));
+
+/*
+=================================
+        testing!
+=================================
+*/
