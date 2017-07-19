6ee6a8a794222dedea3d40d33d67b2d548cfe311
AndriyPolukhin <andriy.polukhin@gmail.com>
2017-07-19 18:02:44 +0300

    just cnsl.log

--- Basic_Algorithms/Algo_15_SeekAndDestroy.js
+++ Basic_Algorithms/Algo_15_SeekAndDestroy.js
@@ -24,7 +24,7 @@ destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 // arr should be modified to exclude the arguments
 // a pseudo code for this is: arr.filter(arg1 and arg2)
 // we have arg[0] and arg[1], so we have arg[i]
-
+/*
 var arr = [1, 2, 3, 1, 2, 3];
 argument[0] = 2;
 argument[1] = 3;
@@ -41,7 +41,7 @@ function destroyer(arr) {
 }
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
-
+*/
 // testing 19.07.2017
 function destroyer(arr) {
     // Remove all the values
@@ -49,6 +49,7 @@ function destroyer(arr) {
     function checkArg(a) {
         for (var i = arguments[0]; i < arguments.length; i++) {
             for (var j = i + 1; j < arguments.length; j++) {
+                debugger;
                 if (arr[i] === i || arr[j] === j) {
                     return false;
                 }
@@ -60,8 +61,11 @@ function destroyer(arr) {
     return result;
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
-
+console.log("Expected [1,1]: " + destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log("Expected [3,5,1]: " + destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+console.log("Expected [1]: " + destroyer([3, 5, 1, 2, 2], 2, 3, 5));
+console.log("Expected [ ]: " + destroyer([2, 3, 2, 3], 2, 3));
+console.log("Expected [ \"hamburger\"]: " + destroyer(["tree", "hamburger", 53], "tree", 53));
 
 // Read this article for more clues
 //https://stackoverflow.com/questions/33686942/javascript-filter-callback-that-uses-arguments
