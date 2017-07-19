e4e7e1b58a6b7baed6526bf60b677f34bf76f06e
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-19 23:00:19 +0300

    solution added

--- Basic_Algorithms/Algo_15_SeekAndDestroy.js
+++ Basic_Algorithms/Algo_15_SeekAndDestroy.js
@@ -7,6 +7,12 @@
 */
 function destroyer(arr) {
     // Remove all the values
+    for (var i = 1; i < arguments.length; i++) {
+        var skip = arguments[i];
+        arr = arr.filter(function (e) {
+            return e !== skip;
+        });
+    }
     return arr;
 }
 
@@ -46,26 +52,84 @@ destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 function destroyer(arr) {
     // Remove all the values
 
-    function checkArg(a) {
-        for (var i = arguments[0]; i < arguments.length; i++) {
-            for (var j = i + 1; j < arguments.length; j++) {
-                debugger;
-                if (arr[i] === i || arr[j] === j) {
-                    return false;
-                }
-                return true;
-            }
-        }
+    /* function checkArg(a) {
+         for (var i = arguments[0]; i < arguments.length; i++) {
+             for (var j = i + 1; j < arguments.length; j++) {
+                 debugger;
+
+                 if (arr[i] === i || arr[j] === j) {
+                     return false;
+                 }
+                 return true;
+             }
+         }
+     }*/
+
+
+    for (var i = 1; i < arguments.length; i++) {
+        var skip = arguments[i];
+        arr = arr.filter(function (e) {
+            return e !== skip;
+        });
     }
-    var result = arr.filter(checkArg);
-    return result;
+    return arr;
 }
 
+
+
+/*   var result = arr.filter(checkArg);
+    return result;
+}
+*/
+
 console.log("Expected [1,1]: " + destroyer([1, 2, 3, 1, 2, 3], 2, 3));
-console.log("Expected [3,5,1]: " + destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+console.log("Expected [1,5,1]: " + destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
 console.log("Expected [1]: " + destroyer([3, 5, 1, 2, 2], 2, 3, 5));
 console.log("Expected [ ]: " + destroyer([2, 3, 2, 3], 2, 3));
 console.log("Expected [ \"hamburger\"]: " + destroyer(["tree", "hamburger", 53], "tree", 53));
 
 // Read this article for more clues
 //https://stackoverflow.com/questions/33686942/javascript-filter-callback-that-uses-arguments
+
+
+// Alternative solutions:
+//https://forum.freecodecamp.org/t/freecodecamp-algorithm-challenge-guide-seek-and-destroy/16046
+
+// Basic solution
+
+function destroyer(arr) {
+
+    var args = Array.prototype.slice.call(arguments);
+
+    for (var i = 0; i < arguments.length; i++) {
+        for (var j = 0; j < args.length; j++) {
+            if (arr[i] === args[j]) {
+                delete arr[i];
+            }
+        }
+    }
+    return arr.filter(Boolean);
+}
+console.log("Expected [ \"hamburger\"]: " + destroyer(["tree", "hamburger", 53], "tree", 53));
+
+
+// Intermediate
+
+function destroyer(arr) {
+    var args = Array.from(arguments).slice(1);
+    return arr.filter(function (val) {
+        return !args.includes(val);
+    });
+}
+console.log("Expected [ \"hamburger\"]: " + destroyer(["tree", "hamburger", 53], "tree", 53));
+
+// More solutions
+function removeItems(arr, items) {
+    return arr.filter(function (elem) {
+        return items.indexOf(elem) === -1;
+    });
+}
+
+var result = removeItems([1, 2, 3, 1, 2, 3, 4], [2, 3]);
+
+console.log(result); // [1, 1, 4]
