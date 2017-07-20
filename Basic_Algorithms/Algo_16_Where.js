887dc74f2ff9dfa96cecf9c398b2f9dc9906bd26
AndriyPolukhin <andriy.polukhin@gmail.com>
2017-07-20 18:01:16 +0300

    test init

--- Basic_Algorithms/Algo_16_Where.js
+++ Basic_Algorithms/Algo_16_Where.js
@@ -4,7 +4,7 @@
 =================================
         My Answer
 =================================
-*/
+
 
 function getIndexToIns(arr, num) {
     // Find my place in this sorted array.
@@ -15,9 +15,18 @@ getIndexToIns([40, 60], 50);
 
 console.log('Should return 3: ' + getIndexToIns([10, 20, 30, 40, 50], 35));
 console.log('Should return 2: ' + getIndexToIns([10, 20, 30, 40, 50], 30));
-
+*/
 /*
 =================================
         testing!
 =================================
 */
+function getIndexToIns(arr, num) {
+    // Find my place in this sorted array.
+    return num;
+}
+
+getIndexToIns([40, 60], 50);
+
+console.log('Should return 3: ' + getIndexToIns([10, 20, 30, 40, 50], 35));
+console.log('Should return 2: ' + getIndexToIns([10, 20, 30, 40, 50], 30));
---
+++ Basic_Algorithms/testing.js
@@ -0,0 +1,4 @@
+function
+
+
+console.log('Expected I\'m A Little Tea Pot : ' + titleCase("I'm a little tea pot"));
