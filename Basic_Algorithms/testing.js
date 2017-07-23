395b73866a9f69e1afc6375701479bdbcfc619ea
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-23 11:17:15 +0300

    key names

--- Basic_Algorithms/testing.js
+++ Basic_Algorithms/testing.js
@@ -1,40 +1,10 @@
-function condenseWords(left, right) {
+camelize = function camelize(str) {
 
-    for (let i = 0; i < left.length; i++) {
-
-        let leftSubstring = left.substring(i);
-
-        if (right.startsWith(leftSubstring)) {
-            return left + right.replace(leftSubstring, '');
-        }
-    }
-    return null;
+    return str.replace(/\W+(.)/g, function (match, chr) {
+        return chr.toUpperCase();
+    });
 }
 
-console.log(condenseWords('live', 'verses'));
-
-// second problem
-
-function condenseSentence(string) {
-
-    const words = string.split(' ');
-
-    for (let i = 0; i < words.length - 1; i++) {
-        //        const left = words[i];
-        //        const right = words[i + 1];
-        const condensed = condenseWords(words[i], words[i + 1]);
-        if (condensed) {
-            words[i] = condensed;
-            words.splice(i + 1, 1);
-            i--;
-        }
-
-    }
-    return words.join(' ');
-
-
-}
-console.log(condenseSentence('no one ever runs so often'));
-console.log(condenseSentence('yellow low dog'));
-console.log(condenseSentence('the black kettle is hot'));
-console.log(condenseSentence('hello world'));
+console.log(camelize('JavaScript Exercises'));
+console.log(camelize('JavaScript exercises'));
+console.log(camelize('JavaScriptExercises'));
