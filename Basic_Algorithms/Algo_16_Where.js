aa2fee352ae1231da24b64d50bf72c898626bee5
AndriyPolukhin <andriy.polukhin@gmail.com>
2017-07-24 17:05:05 +0300

    still testing

--- Basic_Algorithms/Algo_16_Where.js
+++ Basic_Algorithms/Algo_16_Where.js
@@ -30,3 +30,34 @@ getIndexToIns([40, 60], 50);
 
 console.log('Should return 3: ' + getIndexToIns([10, 20, 30, 40, 50], 35));
 console.log('Should return 2: ' + getIndexToIns([10, 20, 30, 40, 50], 30));
+
+
+
+// Testing the algorythm
+function getIndexToIns(arr, num) {
+
+    arr = arr.sort();
+    var arrNum = arr.indexOf(num);
+    var arrCheck = [];
+    var min;
+
+    if (arrNum >= 0) {
+        return arrNum;
+    } else if (arrNum === -1) {
+        for (var i = 0; i < arrNum.lenght; i++) {
+            min = arrNum[i];
+            for (var j = i + 1; j < arrNum.length; j++) {
+                if (min < num && j > num) {
+                    arrCheck.push(min);
+                }
+            }
+        }
+    }
+    console.log(arrCheck);
+
+}
+
+console.log('Should return 1: Got => ' + getIndexToIns([60, 40], 50));
+
+console.log('Should return 2: Got => ' + getIndexToIns([10, 30, 20, 50, 40], 30));
+console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));
--- Basic_Algorithms/testing.js
+++ Basic_Algorithms/testing.js
@@ -1,9 +1,24 @@
 function getIndexToIns(arr, num) {
 
-    console.log(arr.every(i => num >= num + 1));
+    arr = arr.sort();
+    var arrNum = arr.indexOf(num);
+
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+
+            if ((arr[i] < num) && (arr[j] > num)) {
+                return arr[i].index + 1;
+            }
+
+        }
+    }
 }
 
 
-console.log('Should return 1: Got => ' + getIndexToIns([60, 40], 50));
+}
+
+/*console.log('Should return 1: Got => ' + getIndexToIns([60, 40], 50));*/
+
 console.log('Should return 2: Got => ' + getIndexToIns([10, 30, 20, 50, 40], 30));
-console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));
+/*
+console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));*/
