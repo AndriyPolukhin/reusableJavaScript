6b259b82856e74a83c24d74d63f6aa138fe4c7e7
AndriyPolukhin <andriy.polukhin@gmail.com>
2017-07-25 18:52:13 +0300

    progress 8 of 10

--- Basic_Algorithms/Algo_16_Where.js
+++ Basic_Algorithms/Algo_16_Where.js
@@ -89,3 +89,28 @@ function getIndexToIns(arr, num) {
 console.log('Should return 2: Got => ' + getIndexToIns([10, 30, 20, 50, 40], 30));
 /*
 console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));*/
+
+
+// !!!!!!!!!    8 out of 10 !!!!!!!
+function getIndexToIns(arr, num) {
+
+    // Sort the array
+    arr = arr.sort();
+
+    function numIndex(e) {
+        return e >= num;
+    }
+
+    return arr.findIndex(numIndex);
+
+
+}
+
+// So first I loop thorought the array in one of the functions
+// I create a function that checks for ifs
+// I just inject this function into an arr.findIndex(Function);
+
+console.log('Should return 3: Got =>' + getIndexToIns([2, 5, 10], 15));
+
+console.log('Should return 2: Got => ' + getIndexToIns([5, 3, 20, 3], 5));
+console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));
--- Basic_Algorithms/testing.js
+++ Basic_Algorithms/testing.js
@@ -3,41 +3,20 @@ function getIndexToIns(arr, num) {
     // Sort the array
     arr = arr.sort();
 
-    var setnum = num;
-    var ans;
-    var arrNum = arr.indexOf(num);
-
-    // create a function that will chec for the where to place an num
-    function checkNum(num) {
-
-
-        for (var i = 0; i < arr.length; i++) {
-
-
-            for (var j = i + 1; j < arr.length; j++) {
-
-                if ((arr[i] <= setnum) && (arr[j] >= setnum)) {
-                    return ans = arr[j];
-                    console.log('First if ' + ans);
-                } else if (setnum <= arr[i]) {
-
-                    return ans = arr[i];
-                    console.log('Second if ' + ans);
-                }
-            }
-        }
+    function numIndex(e) {
+        return e >= num;
     }
-    console.log('Did you logged me? ' + num + ' and what did you get ' + ans);
-    return arr.findIndex(checkNum);
-}
 
+    return arr.findIndex(numIndex);
+
+
+}
 
 // So first I loop thorought the array in one of the functions
 // I create a function that checks for ifs
 // I just inject this function into an arr.findIndex(Function);
 
-/*console.log('Should return 1: Got => ' + getIndexToIns([60, 40], 50));*/
+console.log('Should return 3: Got =>' + getIndexToIns([2, 5, 10], 15));
 
-console.log('Should return 2: Got => ' + getIndexToIns([10, 30, 20, 50, 40], 30));
-/*
-console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));*/
+console.log('Should return 2: Got => ' + getIndexToIns([5, 3, 20, 3], 5));
+console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));
