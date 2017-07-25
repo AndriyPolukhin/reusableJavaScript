7d7c2731d35aceeeb9193748e7c15150da2ed6a3
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-26 00:47:46 +0300

    Algo_17_init

---
+++ Basic_Algorithms/Algo_17_CaesarsCipher.js
@@ -0,0 +1,17 @@
+// Caesars Cipher
+// link https://www.freecodecamp.com/challenges/caesars-cipher
+// https://www.wikiwand.com/en/ROT13
+
+
+/*
+=================================
+        My Answer
+=================================
+*/
+function rot13(str) { // LBH QVQ VG!
+
+    return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
--- Basic_Algorithms/testing.js
+++ Basic_Algorithms/testing.js
@@ -1,10 +1,16 @@
 function getIndexToIns(arr, num) {
 
     // Sort the array
-    arr = arr.sort();
+    arr.push(num);
+
+    arr.sort(function (a, b) {
+        return a - b;
+    });
 
     function numIndex(e) {
-        return e >= num;
+        if (e) {
+            return e >= num;
+        }
     }
 
     return arr.findIndex(numIndex);
@@ -16,7 +22,7 @@ function getIndexToIns(arr, num) {
 // I create a function that checks for ifs
 // I just inject this function into an arr.findIndex(Function);
 
-console.log('Should return 3: Got =>' + getIndexToIns([2, 5, 10], 15));
+console.log('Should return 3: Got should be last one => ' + getIndexToIns([2, 5, 10], 15));
 
-console.log('Should return 2: Got => ' + getIndexToIns([5, 3, 20, 3], 5));
+console.log('Should return 2:  => ' + getIndexToIns([5, 3, 20, 3], 5));
 console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));
