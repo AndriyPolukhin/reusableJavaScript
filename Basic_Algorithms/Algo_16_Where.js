e9dcc070ffedc75f17c15598879a0c9acb8cf45d
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-07-25 07:46:40 +0300

    still tests

--- Basic_Algorithms/Algo_16_Where.js
+++ Basic_Algorithms/Algo_16_Where.js
@@ -33,7 +33,7 @@ console.log('Should return 2: ' + getIndexToIns([10, 20, 30, 40, 50], 30));
 
 
 
-// Testing the algorythm
+// Testing the algorithem
 function getIndexToIns(arr, num) {
 
     arr = arr.sort();
@@ -61,3 +61,31 @@ console.log('Should return 1: Got => ' + getIndexToIns([60, 40], 50));
 
 console.log('Should return 2: Got => ' + getIndexToIns([10, 30, 20, 50, 40], 30));
 console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));
+
+
+// one more testing
+function getIndexToIns(arr, num) {
+
+    arr = arr.sort();
+    var arrNum = arr.indexOf(num);
+
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+
+            if ((arr[i] < num) && (arr[j] > num)) {
+                return arr[i].index + 1;
+            }
+
+        }
+    }
+}
+
+
+// SO first I loop thorought the array in one of the functions
+// I
+
+/*console.log('Should return 1: Got => ' + getIndexToIns([60, 40], 50));*/
+
+console.log('Should return 2: Got => ' + getIndexToIns([10, 30, 20, 50, 40], 30));
+/*
+console.log('Should return 3: Got => ' + getIndexToIns([20, 10, 50, 40, 30], 35));*/
--- Basic_Algorithms/testing.js
+++ Basic_Algorithms/testing.js
@@ -1,21 +1,40 @@
 function getIndexToIns(arr, num) {
 
+    // Sort the array
     arr = arr.sort();
+
+    var setnum = num;
+    var ans;
     var arrNum = arr.indexOf(num);
 
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = i + 1; j < arr.length; j++) {
+    // create a function that will chec for the where to place an num
+    function checkNum(num) {
 
-            if ((arr[i] < num) && (arr[j] > num)) {
-                return arr[i].index + 1;
+
+        for (var i = 0; i < arr.length; i++) {
+
+
+            for (var j = i + 1; j < arr.length; j++) {
+
+                if ((arr[i] <= setnum) && (arr[j] >= setnum)) {
+                    return ans = arr[j];
+                    console.log('First if ' + ans);
+                } else if (setnum <= arr[i]) {
+
+                    return ans = arr[i];
+                    console.log('Second if ' + ans);
+                }
             }
-
         }
     }
+    console.log('Did you logged me? ' + num + ' and what did you get ' + ans);
+    return arr.findIndex(checkNum);
 }
 
 
-}
+// So first I loop thorought the array in one of the functions
+// I create a function that checks for ifs
+// I just inject this function into an arr.findIndex(Function);
 
 /*console.log('Should return 1: Got => ' + getIndexToIns([60, 40], 50));*/
 
