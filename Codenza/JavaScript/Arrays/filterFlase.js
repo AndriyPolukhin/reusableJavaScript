cf00da885e8341b6d8e5a30963fb50c9d8cb6eb8
AndriyPolukhin <andriy.polukhin@gmail.com>
2017-08-08 10:00:00 +0300

    updated

--- Codenza/JavaScript/Arrays/filterFlase.js
+++ Codenza/JavaScript/Arrays/filterFlase.js
@@ -1,3 +1,4 @@
+/* FILTER AND FALSE ELEMENTS*/
 function filter_array_values(arr) {
     arr = arr.filter(isEligible);
     return arr;
@@ -10,3 +11,17 @@ function isEligible(value) {
 }
 
 console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
+
+
+/* FIND THE NEEDED ELEMENTS */
+function contains(arr, element) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == element) {
+            return true;
+        }
+    }
+    return false;
+}
+var arr = [2, 5, 9, 6];
+
+console.log(contains(arr, 5));
