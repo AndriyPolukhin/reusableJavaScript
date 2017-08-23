86de0b40d34d9e5494b24d89d29e85a5067a676e
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-08-23 23:19:33 +0300

    Armstrong

---
+++ Codenza/PHP/armstrongNumber.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$number = 153;
+$sum = 0;
+$temp = $num;
+while($temp!=0)
+{
+    $rem = $temp%10;
+    $sum = $sum + $rem * $rem * $rem;
+    $temp = $temp/10;
+}
+if($num==$sum)
+{
+    echo "Armstrong number";
+}
+else
+{
+    echo "not an armstrong number";
+}
+
+?>
