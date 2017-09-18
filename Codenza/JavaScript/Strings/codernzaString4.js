ebda7ba8fb0c5acbef0390783ad7dd68382af913
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-09-18 22:25:21 +0300

    unCapitalize strings

--- Codenza/JavaScript/Strings/codernzaString4.js
+++ Codenza/JavaScript/Strings/codernzaString4.js
@@ -22,3 +22,35 @@ function search_word(text, word) {
 
 console.log(search_word("the quick brown fox", 'fox'));
 console.log(search_word("aa, bb, cc, dd, aa", 'aa'));
+
+// XVI  unCapitalize Words
+
+function unCapitalize_Words(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toLowerCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+console.log(unCapitalize_Words('Js String Exercises'));
+console.log(unCapitalize_Words('Take It All To The Next Level'));
+
+
+// XVII  unCapitalize first word
+
+function Uncapitalize(str1) {
+    return str1.charAt(0).toLowerCase() + str1.slice(1);
+}
+
+console.log(Uncapitalize('Js string exercises'));
+console.log(Uncapitalize('This Is Something New'));
+
+// XVIII unCapitalize Words
+
+function unCapitalizeWords(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.substr(0).toLowerCase();
+    });
+}
+
+console.log(unCapitalizeWords('JS EXERCISES STRINGS'));
+console.log(unCapitalizeWords('THIS ALL WAS CAPITALIZE'));
