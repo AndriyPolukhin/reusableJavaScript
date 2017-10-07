ec6547037db38d2fa8621d8a211f218a3a05fdb2
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-10-07 19:45:30 +0300

    stalker 07.10

---
+++ Bootcamp_WD_FS/JavaScript/Stalker/stalker.js
@@ -0,0 +1,6 @@
+var name = prompt("Tell me your name");
+var surname = prompt("Tell me your surname");
+var age = prompt("State your age");
+
+console.log("Your full name is " + name + " " + surname + "!");
+console.log("Your age is " + age);
