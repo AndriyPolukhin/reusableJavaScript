0bc5b0d57c09708faee90e8f8786c7485de7d2ce
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-10-08 19:17:03 +0300

    JS_PS_1

---
+++ Bootcamp_WD_FS/JavaScript/Guessing Game/problemSet1.js
@@ -0,0 +1,42 @@
+/*
+While Loops Problem Set
+1. Print all numbers between -10 and 19;
+2. Print all numbers between 10 and 40;
+3. Print all odd numbers between 300 and 333;
+4. Print all numbers divisible by 5 and 3 between 5 and 50;
+*/
+
+// Solution to problem 1
+var num = -10;
+while (num >= -10 && num <= 19) {
+    console.log(num);
+    num++;
+}
+
+// Solution to problem 2
+var x = 10;
+while (x >= 10 && x <= 40) {
+    if (x % 2 === 0) {
+        console.log(x);
+
+    }
+    x++;
+}
+// Solution to problem 3
+
+var y = 300;
+while (y >= 300 && y <= 333) {
+    if (y % 2 === 1) {
+        console.log(y);
+    }
+    y++;
+}
+
+// Solution to problem 4
+var z = 5;
+while (z >= 5 && z <= 50) {
+    if (z % 5 === 0 && z % 3 === 0) {
+        console.log(z);
+    }
+    z++;
+}
