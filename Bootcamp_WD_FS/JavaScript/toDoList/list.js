b8a9501c2112a620ee4dc17e89fe8ec20c90307e
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-10-14 18:01:19 +0300

    updated todo list

--- Bootcamp_WD_FS/JavaScript/toDoList/list.js
+++ Bootcamp_WD_FS/JavaScript/toDoList/list.js
@@ -3,19 +3,43 @@ var todos = [];
 var input = prompt("What would you like to do?");
 
 
-
 while (input !== "quit") {
     // handle input
-    if (input === "list") {
-        console.log(todos);
-    } else if (input === "new") {
-        // ask for a new todo
-        var newTodo = prompt("Enter a new todo");
-        //add to todos array
-        todos.push(newTodo);
-    }
 
-    // ask again for new input
+    if (input === "list") {
+        listTodos();
+    } else if (input === "new") {
+        addTodo();
+    } else if (input === "delete") {
+        deleteTodo();
+    }
+    // ask againfor new input
     input = prompt("What would you like to do?");
 }
+
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+    console.log("************");
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("******* *****");
+}
+
+function addTodo() {
+    // ask for new todo
+    var newTodo = prompt("Enter  new todo");
+    // add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo() {
+    // ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    // delete that todo
+    // splice()
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+}
