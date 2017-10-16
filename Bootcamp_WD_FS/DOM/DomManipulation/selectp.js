4eb056dc1a4e3f698ca675b49c17aab7f63be36a
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-10-16 15:37:34 +0300

    DOM: selectors

---
+++ Bootcamp_WD_FS/DOM/DomManipulation/selectp.js
@@ -0,0 +1,6 @@
+var p1 = document.getElementById("first");
+var p2 = document.getElementsByClassName("special")[0];
+var p3 = document.querySelector("#first");
+var p4 = document.querySelectorAll("p")[0];
+var p5 = document.getElementsByTagName("p")[0];
+var p6 = document.querySelector(".special");
