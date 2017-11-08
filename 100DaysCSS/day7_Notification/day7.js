4d4975b364fb5b98406eebe00329d554dfd37e2b
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-11-09 00:29:15 +0200

    Day 7:  notification-menu

--- 100DaysCSS/day7_Notification/day7.js
+++ 100DaysCSS/day7_Notification/day7.js
@@ -1,8 +1,7 @@
 $('.search-icon').bind('click', function () {
     $('.search-input').toggleClass('active');
 });
-
 $('.menu-icon').bind('click', function () {
     $('.panel').toggleClass('show-menu');
-    $('.menu').toggleClass("active");
+    $('.menu').toggleClass('active');
 });
