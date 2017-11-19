509ef0ae0a543e8e7c85bcb2d01d997f07c35032
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-11-19 18:42:34 +0200

    updated

--- Codenza/JavaScript/CodeSchoolAjax/script.js
+++ Codenza/JavaScript/CodeSchoolAjax/script.js
@@ -28,3 +28,36 @@ $(document).ready(function () {
         });
     });
 });
+
+
+
+function Tour(el) {
+    var tour = this;
+    this.el = el;
+    this.fetchPhotos = function () {
+        $.ajax('/photos.html', {
+            data: {
+                location: tour.el.data('location')
+            },
+            context: tour,
+            success: function (response) {
+                this.el.find('.photos').html(response).fadeIn();
+            },
+            error: function () {
+                this.el.find('.photos').html('<li>There was a problem fetching the latest photos. Please try again.</li>');
+            },
+            timeout: 3000,
+            beforeSend: function () {
+                this.el.addClass('is-fetching');
+            },
+            complete: function () {
+                this.el.removeClass('is-fetching');
+            }
+        });
+    }
+    this.el.on('click', 'button', this.fetchPhotos);
+}
+$(document).ready(function () {
+    var paris = new Tour($('#paris'));
+    var london = new Tour($('#london'));
+});
