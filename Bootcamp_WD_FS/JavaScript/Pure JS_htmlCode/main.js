0297c4e9c684ef48b1167d3751f82fc18b97e9d3
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-02 19:28:00 +0200

    practice

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
@@ -1,27 +1,3 @@
-function cal_fun() {
-    var psychobox = {
-        school: "Negotiation mastery",
-        place: "Kiev",
-        year: "2002"
-    };
-    var student = {
-        stu1: {
-            name: "Andriy",
-            status: "Brilliant"
-        },
-        stu2: {
-            name: "Sergey",
-            status: "Undergraduate"
-        },
-        stu3: {
-            name: "Alexander",
-            status: "Genius"
-        }
-    }
-
-    document.getElementById("d1").innerHTML = "School Name: " + psychobox.school + "School Place: " + psychobox.place + " Year: " + psychobox.year;
-
-    document.getElementById("d2").innerHTML = "Name 1 : " + student.stu1.name + "place : " + student.stu3.status;
-
-
-}
+(function () {
+    console.log("Andriy and Anastasia Passionatly Kiss each other")
+})();
