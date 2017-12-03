77c9f840e19052da3fe5bd3779b33f2233d92d10
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-03 18:20:17 +0200

    js : practice

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
@@ -1,3 +1,13 @@
-(function () {
-    console.log("Andriy and Anastasia Passionatly Kiss each other")
-})();
+function inArray(arrayToCheck, value) {
+    for (i = 0; i < arrayToCheck.length; i++) {
+        if (arrayToCheck[i] === value) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+var randArray = [1, 2, 3, 4];
+
+document.write("In Array : ", inArray(randArray, 4) + "<br>");
