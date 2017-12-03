c79adf837b2de421a767742e678bf2205c75d617
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-03 18:29:44 +0200

    pr js: func

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
@@ -1,13 +1,19 @@
+// init a fun() with placeholders [holder] and [holder[value]]
 function inArray(arrayToCheck, value) {
-    for (i = 0; i < arrayToCheck.length; i++) {
-        if (arrayToCheck[i] === value) {
+    // init a for loop for [holder]
+    for (j = 0; j < arrayToCheck.length; j++) {
+        // init a check for the [holder[value]]
+        if (arrayToCheck[j] === value) {
+            // finish with true if found
             return true;
         }
     }
-
+    // finish with false if !found
     return false;
 }
 
-var randArray = [1, 2, 3, 4];
+// init an random array to hold data
+var randArray = [1, 2, 3, 4, 5, 6];
 
-document.write("In Array : ", inArray(randArray, 4) + "<br>");
+// Output the functionn check
+document.write(inArray(randArray, 4) + "<br>");
