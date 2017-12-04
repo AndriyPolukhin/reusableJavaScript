072e4ded9329d19219a88a8a4887ef38cbb58364
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-04 20:34:08 +0200

    js: practice objects

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
@@ -1,39 +1,34 @@
-var pElements = document.getElementsByTagName("p");
-
-pElements[1].style.backgroundColor = "#edFeEd";
-
-document.childNodes[1].style.backgroundColor = "#FAE8D7";
-
-var samplDiv2 = document.getElementById("samplDiv2");
-
-samplDiv2.childNodes[1].style.backgroundColor = "#F0FF00";
-
-samplDiv2.childNodes[2].childNodes[1].style.backgroundColor = "#BFAFB2";
+function Customer(name, street, city, state, email, balance) {
+    this.name = name;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.email = email;
+    this.balance = balance;
 
 
-document.write("Node Type : ", samplDiv2.childNodes[1].childNodes[0].nodeType, "<br>");
-document.write("Node Name : ", samplDiv2.childNodes[1].childNodes[1].nodeName, "<br>");
+    this.payDown = function (amtPaid) {
+        this.balance -= amtPaid;
+    };
+    this.addToBal = function (amtCharged) {
+        this.balance += amtCharged;
+    };
 
-var logo = document.getElementById("logo2");
-
-document.write("Logo has alt : ", logo.hasAttribute("alt"), "<br>");
-
-logo.setAttribute("alt", "HELLO", "<br>");
-
-document.write("Get Attributte : ", logo.getAttribute("alt"), "<br>");
-
-
-var attribList = document.getElementById("logo2").attributes;
-
-for (i = 0; i < attribList.length; i++) {
-    document.write("Attribute : ", i, " : ", attribList[i].nodeName, " : ", attribList[i].nodeValue, "<br>");
 }
 
 
-var paragraph3 = document.createElement("p");
+var cust1 = new Customer("Andriy Polukhin", "Achtern Hoagen 2", "Hamburg", "Rossengaten", "andriy.polukhin@gmail.com", 8000000.00);
 
-paragraph3.setAttribute("id", "paragraph3");
-paragraph3.innerHTML = "THIS IS A NEW PARAGRAPH WITH A TEXT GENERATED WITH A USE OF JAVASCRIPT";
 
-samplDiv2.appendChild(paragraph3);
-samplDiv2.insertBefore(paragraph3, samplDiv2.childNodes[0]);
+var cust2 = new Customer("Anastasia Tsukrova", "Achtern Hoagen 2", "Hamburg", "Rossengaten", "stasy@bigmir.net", 2000000.00);
+
+cust2.addToBal(15.50);
+
+Customer.prototype.isCreditAvail = true;
+
+Customer.prototype.toString = function () {
+    return this.name + " lives at " + this.street + " in " + this.city + " " + this.state + " email : " + this.email + " and has a balance of $" + this.balance.toFixed(2) + " Creditworthy : " + this.isCreditAvail + "<br>";
+};
+
+document.write(cust1.toString());
+document.write(cust2.toString());
