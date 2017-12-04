3960672da753617c681c87f050cfc22a233a6530
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-04 20:09:20 +0200

    js:practice DOM

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
@@ -1,19 +1,39 @@
-document.getElementById("chgBkColor").onclick = function (event) {
-    document.getElementById("samplDiv").style.backgroundColor = "#EFDECD";
+var pElements = document.getElementsByTagName("p");
+
+pElements[1].style.backgroundColor = "#edFeEd";
+
+document.childNodes[1].style.backgroundColor = "#FAE8D7";
+
+var samplDiv2 = document.getElementById("samplDiv2");
+
+samplDiv2.childNodes[1].style.backgroundColor = "#F0FF00";
+
+samplDiv2.childNodes[2].childNodes[1].style.backgroundColor = "#BFAFB2";
+
+
+document.write("Node Type : ", samplDiv2.childNodes[1].childNodes[0].nodeType, "<br>");
+document.write("Node Name : ", samplDiv2.childNodes[1].childNodes[1].nodeName, "<br>");
+
+var logo = document.getElementById("logo2");
+
+document.write("Logo has alt : ", logo.hasAttribute("alt"), "<br>");
+
+logo.setAttribute("alt", "HELLO", "<br>");
+
+document.write("Get Attributte : ", logo.getAttribute("alt"), "<br>");
+
+
+var attribList = document.getElementById("logo2").attributes;
+
+for (i = 0; i < attribList.length; i++) {
+    document.write("Attribute : ", i, " : ", attribList[i].nodeName, " : ", attribList[i].nodeValue, "<br>");
 }
 
-document.getElementById("chgBkImg").onclick = function (event) {
-    document.getElementById("samplDiv").style.backgroundImage = "url('m.jpg')";
-}
 
-document.getElementById("chgBorderStyle").onclick = function (event) {
-    document.getElementById("samplDiv").style.borderStyle = "solid";
-}
+var paragraph3 = document.createElement("p");
 
-document.getElementById("chgBorderWidth").onclick = function (event) {
-    document.getElementById("samplDiv").style.borderWidth = "thick";
-}
+paragraph3.setAttribute("id", "paragraph3");
+paragraph3.innerHTML = "THIS IS A NEW PARAGRAPH WITH A TEXT GENERATED WITH A USE OF JAVASCRIPT";
 
-document.getElementById("chgBorderColor").onclick = function (event) {
-    document.getElementById("samplDiv").style.borderColor = "blue";
-}
+samplDiv2.appendChild(paragraph3);
+samplDiv2.insertBefore(paragraph3, samplDiv2.childNodes[0]);
