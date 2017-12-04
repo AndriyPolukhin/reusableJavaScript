fc5186edc81546ebf1fc05d5b0d8411250fa49e0
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-04 20:48:03 +0200

    js: practice validation

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
@@ -1,34 +1,21 @@
-function Customer(name, street, city, state, email, balance) {
-    this.name = name;
-    this.street = street;
-    this.city = city;
-    this.state = state;
-    this.email = email;
-    this.balance = balance;
-
-
-    this.payDown = function (amtPaid) {
-        this.balance -= amtPaid;
-    };
-    this.addToBal = function (amtCharged) {
-        this.balance += amtCharged;
-    };
-
+function editNodeText(regex, input, helpId, helpMessage) {
+    if (!regex.test(input)) {
+        if (helpId != null) {
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+            helpId.appendChild(document.createTextNode(helpMessage));
+        }
+    } else {
+        if (helpId != null) {
+            while (helpId.childNodes[0]) {
+                helpId.removeChild(helpId.childNodes[0]);
+            }
+        }
+    }
 }
 
 
-var cust1 = new Customer("Andriy Polukhin", "Achtern Hoagen 2", "Hamburg", "Rossengaten", "andriy.polukhin@gmail.com", 8000000.00);
-
-
-var cust2 = new Customer("Anastasia Tsukrova", "Achtern Hoagen 2", "Hamburg", "Rossengaten", "stasy@bigmir.net", 2000000.00);
-
-cust2.addToBal(15.50);
-
-Customer.prototype.isCreditAvail = true;
-
-Customer.prototype.toString = function () {
-    return this.name + " lives at " + this.street + " in " + this.city + " " + this.state + " email : " + this.email + " and has a balance of $" + this.balance.toFixed(2) + " Creditworthy : " + this.isCreditAvail + "<br>";
-};
-
-document.write(cust1.toString());
-document.write(cust2.toString());
+function isTheFieldEmpty(inputField, helpId) {
+    return editNodeText(/^[A-Za-z\,\'-]{1,15}\s?[A-Za-z\.\'\-]{1,15}\s?[A-Za-z\,\' \-]{1,15}/, inputField.value, helpId, "Please enter a valid name");
+}
