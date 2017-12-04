51770b65c172a581cde8f53fe1c347a6dba4ec42
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-04 17:11:24 +0200

    js:practice

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/main.js
@@ -1,9 +1,31 @@
-function factorial(num) {
-    if (num <= 1) {
-        return 1;
+// Init a Function of events
+function getChar(event) {
+    // for IE this is the code goes here.
+    if (event.which === null) {
+        // if not null then return String
+        return String.fromCharCode(event.keyCode);
+
+        // for all other browsers.
+    } else if (event.which != 0 && event.charCode != 0) {
+        return String.fromCharCode(event.which);
     } else {
-        return num * factorial(num - 1);
+        return null;
     }
 }
 
-document.write("Return a factorial of a 5 = ", factorial(5), "<br>");
+
+// now I call for the characters
+document.getElementById("charInput").onkeypress = function (event) {
+
+    // init a var and add some safeguards for older browser
+    var char = getChar(event || window.event);
+
+    // check if the valid char pressed
+    if (!char) {
+        return false;
+    }
+
+    // return the data into the field
+    document.getElementById("keyData").innerHTML = char + " was clicked";
+    return true;
+}
