b182ba5a9e24aca3a14a1ad4d89e9c8792773e18
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-07 14:41:08 +0200

    design patterns: observable

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/ooMain.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/ooMain.js
@@ -1,24 +1,76 @@
-// Here I initiate a Pizza object
+// OBSERVOR PATTERN
 
-function Pizza(price) {
-    this.price = price || 10;
+
+// initialte and Observalbe function.
+var Observable = function () {
+
+    // here I add what I observe
+    this.subscribers = [];
 }
 
-// I add to the function via prototype my function get price
-Pizza.prototype.getPrice = function () {
-    return this.price;
+
+// Create a prototype with different functions
+Observable.prototype = {
+
+    // how to subsribe
+    subscribe: function (subscriber) {
+        // I just push the data into an Array
+        this.subscribers.push(subscriber);
+    },
+
+    // how to unsubscribe 
+    unsubscribe: function (unsubscriber) {
+
+        // in order ot exclude data I need to find it inArray via the for loop
+        for (i = 0; i < this.subscribers.length; i++) {
+
+
+            // So first of all the loop checks is the [i] is in the Array
+            if (this.subscribers[i] === unsubscriber) {
+                // after the [i] is found it is excluded via splice(i,1)
+                this.subscribers.splice(i, 1);
+
+                // also the function retuns what it did
+                return unsubscriber.name;
+            }
+        }
+    },
+
+
+    // in order to send data to all of the subscribers we use this function
+    publish: function (data) {
+
+        // we use a loop to cycle thorugh them
+        for (i = 0; i < this.subscribers.length; i++) {
+            // adda funciton that sends the provided data
+            this.subscribers[i].receiveData(data);
+        }
+    }
+};
+
+var OrganFanny = {
+    name: "Organ Fanny",
+    receiveData: function (data) {
+        document.write(this.name + " received your info " + data + "<br>");
+    }
 }
 
-// Here I modify the pizza with extra adds
-// Just add some cheese
-function ExtraCheese(pizza) {
-    var prevPrice = pizza.price;
-
-    pizza.price = prevPrice + 1;
+var BoldmanYaks = {
+    name: "Boldman Yaks",
+    receiveData: function (data) {
+        document.write(this.name + " received your info " + data + "<br>");
+    }
 }
 
-var myPizza = new Pizza(14);
 
-ExtraCheese(myPizza);
+observable = new Observable();
 
-document.write("Cost of Pizza : $" + myPizza.price + "<br>");
+observable.subscribe(OrganFanny);
+observable.subscribe(BoldmanYaks);
+
+
+observable.publish("IBM AT $ 145.39");
+
+document.write(observable.unsubscribe(OrganFanny) + "unsubscribed<br>");
+
+observable.publish("IBM AT $ 145.87");
