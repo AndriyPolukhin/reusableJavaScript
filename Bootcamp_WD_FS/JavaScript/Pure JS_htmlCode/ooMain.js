cab767266d2d8ddefc1841f82b4093c5cd72f82c
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-07 13:59:20 +0200

    Factory Design Pattern

--- Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/ooMain.js
+++ Bootcamp_WD_FS/JavaScript/Pure JS_htmlCode/ooMain.js
@@ -1,56 +1,82 @@
-var Observable = function () {
-    this.subscribers = [];
-
+// Initiate functions that represent Type of weapons
+function Sword(desc) {
+    this.weaponType = "Sword";
+    this.material = desc.material || "Steel";
+    this.style = desc.style || "Longsword";
+    this.hasMagic = desc.hasMagic || false;
 }
 
-Observable.prototype = {
-    subscribe: function (subscriber) {
-        this.subscribers.push(subscriber);
-    },
-
-    unsubscribe: function (unsubscriber) {
-        for (i = 0; i < this.subscribers.length; i++) {
-            if (this.subscribers[i] === unsubscriber) {
-                this.subscribers.splice(i, 1);
-
-                return unsubscriber.name;
-            }
-        }
-    },
-
-    publish: function (data) {
-        for (i = 0; i < this.subscribers.length; i++) {
-            this.subscribers[i].receiveData(data);
-        }
-    }
-};
-
-
-var OrganFanny = {
-    name: "Organ Fanny",
-    receiveData: function (data) {
-        document.write(this.name + " received your info " + data + "<br>");
-    }
+function Bow(desc) {
+    this.weaponType = "Bow";
+    this.material = desc.material || "Wood";
+    this.style = desc.style || "Longbow";
+    this.hasMagic = desc.hasMagic || false;
 }
 
-
-var BoldmanYaks = {
-    name: "Boldman Yaks",
-    receiveData: function (data) {
-        document.write(this.name + " received your info " + data + "<br>");
-    }
+// Here I've created a new function of Shield
+// The function takes a description of an object that I want to create.
+function Shield(desc) {
+    // I assign a Type of the object
+    this.weaponType = "Shield";
+    // Here are some parameters holders
+    // this are optional
+    this.material = desc.material || "Steel";
+    this.style = desc.style || "LongShield";
+    // this one has boolean logic
+    this.hasMagic = desc.hasMagic || false;
 }
 
-
-var observable = new Observable();
-
-observable.subscribe(OrganFanny);
-observable.subscribe(BoldmanYaks);
+// Now for the creationo of the factory.
+// Here I init a function for the Weapon Factory
+function WeaponFactory() {};
 
 
-observable.publish("IBM at $ 15" + "<br>");
+// Here the modification of the prototype stars.
+// First add some actual functions to make weapons
+WeaponFactory.prototype.makeWeapon = function (desc) {
+    // init a null variable
+    var weaponClass = null;
 
+    // logic to check for the type of weapon we are making
+    if (desc.weaponType === "Sword") {
+        // Set the class equal to Type
+        weaponClass = Sword;
 
-document.write(observable.unsubscribe(OrganFanny) + "<br>");
+    } else if (desc.weaponType === "Bow") {
+        weaponClass = Bow;
+    } else if (desc.weaponType === "Shield") {
+        weaponClass = Shield;
+    } else {
+        // nothing added this will give a false
+        return false;
+    }
 
-observable.publish("IBM at $ 25" + "<br>");
+    // so now we can return a new weapon class(desc) ???
+    return new weaponClass(desc);
+}
+
+// Practice on an actual factory and variable.
+
+// init a variable for factory
+var myWeaponFact = new WeaponFactory();
+
+// init an object for weapon
+var bladeFist = myWeaponFact.makeWeapon({
+    weaponType: "Sword",
+    material: "Dark Iron",
+    style: "Scythe",
+    hasMagic: true
+});
+
+document.write(bladeFist.weaponType + "of type " + bladeFist.style + " crafted from " + bladeFist.material + "<br>");
+
+var shieldWeaponFact = new WeaponFactory();
+
+var superShield = shieldWeaponFact.makeWeapon({
+    weaponType: "Shield",
+    material: "Dragon Glass",
+    style: "Two handed",
+    hasMagic: true
+});
+
+document.write(superShield.weaponType + " of type " + superShield.style + " crafted from " + superShield.material + "<br>");
