e3339c32d24f6b7bd22c52a096cb692614351b72
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-18 19:27:15 +0200

    practice js

---
+++ Codenza/JavaScript/jQuery/practice.js
@@ -0,0 +1,25 @@
+var counter = (function () {
+    var privateCounter = 0;
+
+    function changeBy(val) {
+        privateCounter += val;
+    }
+    return {
+        increment: function () {
+            changeBy(1);
+        },
+        decrement: function () {
+            changeBy(-1);
+        },
+        value: function () {
+            return privateCounter;
+        }
+    };
+})();
+
+console.log(counter.value());
+counter.increment();
+counter.increment();
+console.log(counter.value());
+counter.decrement();
+console.log(counter.value());
