402a6bd2e914d269fbee86dc05379e77391ea143
Andriy Polukhin <andriy.polukhin@gmail.com>
2017-12-19 17:39:10 +0200

    Symbols

--- Codenza/JavaScript/jQuery/practice.js
+++ Codenza/JavaScript/jQuery/practice.js
@@ -1,10 +1,37 @@
-var start = new Date();
-doSomething();
-var end = new Date();
+// Use case 1: Symbols as property keys
 
-var elapsed = end.getTime() - start.getTime();
-console.log(elapsed);
+const MY_KEY = Symbol();
+let obj = {};
 
-function doSomething() {
-    for (var i = 0; i < 10000000000; i++) {}
-};
+obj[MY_KEY] = 123;
+console.log(obj[MY_KEY]);
+
+
+// Use case 2: constants representing concepts
+const COLOR_RED = Symbol('Red');
+const COLOR_ORANGE = Symbol('Orange');
+const COLOR_YELLOW = Symbol('Yellow');
+const COLOR_GREEN = Symbol('Green');
+const COLOR_BLUE = Symbol('Blue');
+const COLOR_VIOLET = Symbol('Violet');
+
+function getComplement(color) {
+    switch (color) {
+        case COLOR_RED:
+            return COLOR_GREEN;
+        case COLOR_ORANGE:
+            return COLOR_BLUE;
+        case COLOR_YELLOW:
+            return COLOR_VIOLET;
+        case COLOR_GREEN:
+            return COLOR_RED;
+        case COLOR_BLUE:
+            return COLOR_ORANGE;
+        case COLOR_VIOLET:
+            return COLOR_YELLOW;
+        default:
+            return ('Unknown color: ' + color);
+    }
+}
+
+console.log(getComplement('Red'));
