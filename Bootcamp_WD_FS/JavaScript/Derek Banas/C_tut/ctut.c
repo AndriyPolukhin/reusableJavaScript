2c3fefdbbc50aba78e23ef9bca7a352d433a455a
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-01-15 16:01:33 +0200

    google api practice

--- Bootcamp_WD_FS/JavaScript/Derek Banas/C_tut/ctut.c
+++ Bootcamp_WD_FS/JavaScript/Derek Banas/C_tut/ctut.c
@@ -1,40 +1,28 @@
-// Floating-point arithmetic
-
 // #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-float quarter = 25.00, dime = 10.00, nickel = 5.00, penny = 1.00;
+long long ifValid(long long card);
+
+string status = "INVALID";
 
 int main(void)
 {
 
     // Prompt user for cash
-    float cash;
+    long long card;
     do
     {
-        cash = get_float("How much do should I give you? ");
-    } while (cash <= 0);
+        card = get_long_long("Change owned: ");
+    } while (card < 999999999999);
 
-    // Formula
-    float chAmt = round(cash * 100);
-    float coins = calculateCoins(chAmt);
-    printf("Amount of Coins is: %.2f", coins);
-
-    printf("Float is %f\n", cash);
-    printf("float is %.2f\n", round(cash * 100));
+    long long checkNum = ifValid(card);
+    printf("%s and %lld\n", status, checkNum);
+    return 0;
 }
 
-float calculatecouns(chAmt)
+long long ifValid(long long card)
 {
-    int count = 0;
-    float a = chAmt;
-    while (a > quarter)
-    {
-        count++;
-        return a = a - quarter;
-    }
-
-    printf("Change Amount %.2f", a);
-    printf("count is %i\n", count);
+    printf("%s and %lld\n", status, card);
 }
