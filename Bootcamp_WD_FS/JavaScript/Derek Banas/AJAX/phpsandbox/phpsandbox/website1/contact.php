ee107795c03a8f730476d30c0d55978a8e76dbf2
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-01-20 21:03:40 +0200

    phpsandbox update

---
+++ Bootcamp_WD_FS/JavaScript/Derek Banas/AJAX/phpsandbox/phpsandbox/website1/contact.php
@@ -0,0 +1,5 @@
+<?php include 'inc/header.php'; ?>
+
+<h1>Contact</h1>        
+ 
+<?php include 'inc/footer.php'; ?>
---
+++ Bootcamp_WD_FS/JavaScript/Derek Banas/AJAX/phpsandbox/phpsandbox/website1/inc/footer.php
@@ -0,0 +1,7 @@
+<footer>
+<p>Mywebsite &copy; 2018</p>
+</footer>
+
+</body>
+</html>
+
