01bc22dd4efc6f8a03ea35689d0e67c9066eb594
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-02-02 21:49:27 +0200

    linked list in c

--- Bootcamp_WD_FS/JavaScript/Derek Banas/C_tut/ctut.c
+++ Bootcamp_WD_FS/JavaScript/Derek Banas/C_tut/ctut.c
@@ -1,35 +1,153 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct dogsFavs 
+struct product 
 {
-    char *food;
-    char *friend;
+    float price;
+    char productName[30];
+
+    struct product *next;
 };
 
-typedef struct dog
+struct product *pFirstNode = NULL;
+struct product *pLastNode = NULL;
+
+
+void createNewList()
 {
-    const char *name;
-    const char *beed;
-    int avgHeightCm;
-    int avgWeightLbs;
+    struct product *pNewStruct = (struct product *) malloc(sizeof(struct product));
 
-    struct dogsFavs favoriteThings;
+    pNewStruct->next = NULL;
 
-} dog;
+    printf("Enter Product Name: ");
+    scanf("%s", &(pNewStruct)->productName);
 
-void getDogsFavs(dog theDog)
-{
-    printf("\n");
+    printf("Enter Product price: ");
+    scanf("%f", &(pNewStruct)->price);
 
-    printf("%s lovaes %s and his friend is %s\n\n", 
-        theDog.name,
-        theDog.favoriteThings.food,
-        theDog.favoriteThings.friend);
+    pFirstNode = pLastNode = pNewStruct;
 }
 
-void main()
-{
-    dog benji = {"Benji", "Silky Terrier", 25, 9, {"Meat", "Joe Camp"}};
 
-    getDogsFavs(benji);
+void outputData()
+{
+    struct product *pProducts = pFirstNode;
+
+    printf("Products Entered\n\n");
+
+    while(pProducts != NULL)
+    {
+        printf("%s cost %.2f\n\n", pProducts->productName,
+            pProducts->price);
+
+            pProducts = pProducts->next;
+    }
+}
+
+
+void inputData()
+{
+    if(pFirstNode == NULL)
+    {
+        createNewList();
+    } else {
+        
+
+        struct product *pNewStruct = (struct product *) malloc(sizeof(struct product));
+
+        printf("Enter Product Name: ");
+        scanf("%s", &(pNewStruct)->productName);
+
+        printf("Enter Product price: ");
+        scanf("%f", &(pNewStruct)->price);
+
+        if(pFirstNode == pLastNode)
+        {
+            pFirstNode->next = pNewStruct;
+
+            pLastNode = pNewStruct;
+
+            pNewStruct->next = NULL;
+        } else {
+
+            pLastNode->next = pNewStruct;
+
+            pNewStruct->next = NULL;
+
+            pLastNode = pNewStruct;
+
+
+        }
+    }
+}
+
+struct product *pProductBeforeProductToDelete = NULL;
+
+struct product* searchForProduct(char * productName)
+{
+    struct product *pProductIterator = pFirstNode;
+
+    while(pProductIterator != NULL)
+    {
+        int areTheyEqual = strncmp(pProductIterator->productName,
+            productName, 30);
+
+            if(!areTheyEqual) 
+            {
+                printf("%s was found and it costs %.2f\n\n",
+                    pProductIterator->productName,
+                    pProductIterator->price);
+
+                    return pProductIterator;
+            }
+           
+            pProductBeforeProductToDelete = pProductIterator;
+            pProductIterator = pProductIterator->next;
+    }
+    printf("%s Wasn't Found\n\n",  productName);
+    return NULL;
+}
+
+
+void removeProduct(char * productName)
+{
+    struct product *pProductToDelete = NULL;
+
+    pProductToDelete = searchForProduct(productName);
+
+    if(pProductToDelete != NULL)
+    {
+        printf("%s Was Deleted\n\n", productName);
+
+        if(pProductToDelete == pFirstNode)
+        {
+            pFirstNode = pProductToDelete->next;
+        } else {
+            pProductBeforeProductToDelete->next = pProductToDelete->next; 
+        }
+
+        free(pProductToDelete);
+    } else {
+        printf("%s Was Not Found", productName);
+    }
+}
+
+
+
+int main()
+{
+
+    inputData();
+    inputData();
+    inputData();
+
+    outputData();
+
+    removeProduct("Tomato");
+    searchForProduct("Tomato");
+
+    outputData();
+
+    return 0;
 }
