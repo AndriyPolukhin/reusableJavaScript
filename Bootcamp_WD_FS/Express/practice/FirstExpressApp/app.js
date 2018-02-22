7119d17c712b3a9b67c199d69280bc14d74eb2f1
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-02-22 21:50:38 +0200

    express practice

---
+++ Bootcamp_WD_FS/Express/practice/FirstExpressApp/app.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var app = express();
+
+
+// 0. If I shift the "*" to the begining than all of the other routes do not work
+// app.get("*", function(req, res) {
+//     console.log("NO DATA BESIDES THIS ONE");
+//     res.send("NO DATA BESIDES THIS ONE IS PROVIDED BY THE ROUTE");
+// });
+
+// 1. "/" => "Hi There!"
+app.get("/",  function(req, res) {
+    res.send("Hi there LOCALHOST!");
+});
+
+// 2. "/bye" => "Goodbye!"
+app.get("/bye", function(req, res) {
+    res.send("Good Bye at the LOCALHOST");
+});
+
+// 3. "/dog" => "MEOW!"
+
+app.get("/dog", function(req, res) {
+    console.log('I MADE A REQUEST TO THE DOG PAGE');
+    res.send("MEOW from the LOCALHOST");
+});
+
+
+// 4. Let's learn the parameters usage
+
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to the page about " + subreddit.toUpperCase() + " Subreddit");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function (req, res) {
+    console.log(req.params);
+    var subreddit = req.params.subredditName;
+    var id = req.params.id;
+    var title = req.params.title;
+    res.send("Welcome to the page about " + subreddit + "Under the id " + id + " Titled: " + title);
+});
+
+// 5. All of the rest that are not actual routes
+
+app.get("*", function (req, res) {
+    res.send("THIS IS NOT an Actual Route. Please check the page address!");
+});
+
+// 6. "Tell Express to listen for request (start server)"
+
+app.listen(3000, function() {
+    console.log("Serving the text on the LOCALHOST");
+}); 
