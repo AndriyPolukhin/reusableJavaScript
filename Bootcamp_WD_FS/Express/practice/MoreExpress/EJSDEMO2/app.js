9c05e0c6884ead5b3113918959218e2a93429e17
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-02-26 20:53:35 +0200

    partials express

--- Bootcamp_WD_FS/Express/practice/MoreExpress/EJSDEMO2/app.js
+++ Bootcamp_WD_FS/Express/practice/MoreExpress/EJSDEMO2/app.js
@@ -1,17 +1,19 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-    
-    res.render("home.ejs");
-    // res.send("<h1>Thi s is the main page</h1><h2>WELCOME</h2>");
+app.get("/", function (req, res) {    
+    res.render("home");
 });
+
+
 
 
 app.get("/fallinlovewith/:thing", function(req, res) {
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar:thing});
+    res.render("love", {thingVar:thing});
 });
 
 app.get("/posts", function(req, res) {
@@ -21,7 +23,7 @@ app.get("/posts", function(req, res) {
         {title: "Maried Anastasia", author: "Andruha"},
     ];
 
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 
