fed102739ca144e074a1c2d686fd3be898203762
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-03-03 14:50:24 +0200

    post express app done!

--- Bootcamp_WD_FS/Express/practice/MoreExpress/PostRequestDemo2/app.js
+++ Bootcamp_WD_FS/Express/practice/MoreExpress/PostRequestDemo2/app.js
@@ -1,13 +1,29 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 
+var friends = ["Andriy", "Nadia", "Antony", "Olga", "Anastasia"];
 
 
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/friends", function (req, res) {    
+    res.render("friends", {friends: friends});
+});
 
 
+app.post("/addfriend", function (req, res) {
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
 
 
 
