f9b7d11a96ba71487f294308209c0eecb0fd31f9
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-03-03 14:22:55 +0200

    express init project

---
+++ Bootcamp_WD_FS/Express/practice/MoreExpress/PostRequestDemo2/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("SERVER IS LISTENING");
+});
