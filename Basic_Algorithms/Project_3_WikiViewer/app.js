dadae74cea993fd8cd3b1ff558afd4a67e682483
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-03-07 00:08:30 +0200

    WikiViewer init files

---
+++ Basic_Algorithms/Project_3_WikiViewer/app.js
@@ -0,0 +1,9 @@
+// Assignment: Build a Wikipedia Viewer
+
+// 1. I can click a button to see a random Wikipedia entry. 
+// (Implemented in index.html with a href to https://en.wikipedia.org/wiki/Special:Random)
+
+// 2. I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.
+    // 2.1 Change the Icon to a search input with a close span
+    // 2.2 Load the articles with a Wikipedia API as a list
+    // 2.3 Style the list and the App itself in css
