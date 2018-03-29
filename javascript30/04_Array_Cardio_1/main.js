7f23bb15b91285a217404a4a9ba8b0864002dba7
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-03-29 23:37:11 +0300

    cardio start

---
+++ javascript30/04_Array_Cardio_1/main.js
@@ -0,0 +1,48 @@
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+// Some data we can work with
+
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1867, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed:1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852},
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905},
+    { first: 'Lise', last:'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hamarstrom', year: 1829, passed: 1909 },
+    { first: 'Andriy', last: 'Polukhin', year: 1985, passed: 0000 },
+    { first: 'Anastasia', last: 'Polukhina (Tsukrova)', year: 1989, passed: 0000}
+];
+
+ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William', 'Andriy, Polukhin', 'Anastasia, Polukhina'];
+
+ //Array.prototype.filter()
+ // 1. Filter the list of inventros for those who were born in the 1550's
+
+ // Array.prototype.map()
+ // 2. Give us an array of the inventrors' first and last names
+
+ // Array.prototype.sort()
+ // 3. Sort the inventors by birthdate, oldest to youngetst
+
+ // Array.prototype.reduce()
+ // 4. How many year did all the inventors live?
+ 
+
+ // 5. Sort the inventors by year lived
+
+ // 6. Create a list of Boulevards in Paris that contain 'de' anywhere in the name
+ // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+
+ // 7. Sort Exercise
+ // Sort the people alphabeticaly by last name
+
+ //8. Reduce Exercise
+ // Sum the instances of each of therse
+ const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
