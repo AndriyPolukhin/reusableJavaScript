5a0602bf74d8154399f21dd9653900768f42280c
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-05-11 22:51:51 +0300

    versions

--- practical_js/objects-3.js
+++ practical_js/objects-3.js
@@ -1,6 +1,28 @@
-var andriy = {
-    name: 'Andriy',
-    sayName: function () {
-        console.log(this.name);
+// Store in an array
+// var todos = ['item 1', 'item 2', 'item 3'];
+
+// Store in an object
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+
+    // Display
+    displayTodos: function () {
+        console.log('My Todos', this.todos);
+    },
+    // Add
+    addTodo: function (todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    //Change
+    changeTodo: function (position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    //Delete
+    deleteTodo: function (position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
     }
-}
+
+};
