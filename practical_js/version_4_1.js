0ae2eb8d1c76361e7689a2048f81c4567ff8fe81
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-05-12 10:56:51 +0300

    verions 5 and 6

---
+++ practical_js/version_4_1.js
@@ -0,0 +1,26 @@
+var todoList = {
+    todos: [],
+    displayTodos: function () {
+        console.log('My Todos', this.todos);
+    },
+    addTodo: function (todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function (position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function (position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function (position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+};
