d9ac141e56992492494d6d238a6d867ddad75e93
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-05-19 23:45:44 +0300

    es 6 practice

---
+++ es6_19/data.js
@@ -0,0 +1,84 @@
+/* 
+data that I can use for practice
+*/
+
+const inventors = [{
+        first: 'Albert',
+        last: 'Einstein',
+        year: 1879
+    },
+    {
+        first: 'Isaac',
+        last: 'Newton',
+        year: 1643
+    },
+    {
+        first: 'Galileo',
+        last: 'Lalilei',
+        year: 1564
+    },
+    {
+        first: 'Marie',
+        last: 'Curie',
+        year: 1867
+    },
+    {
+        first: 'Johannes',
+        last: 'Kepler',
+        year: 1571
+    },
+    {
+        first: 'Nicolaus',
+        last: 'Copernicus',
+        year: 1473
+    },
+    {
+        first: 'Max',
+        last: 'Planck',
+        year: 1858
+    }
+];
+
+
+const beers = [{
+        name: 'Purple Iris',
+        abv: 6.8,
+        label: 'https://s3.amazonaws.com/brewerydbapi/beer/dMLwGo/upload_yiUllE-large.png',
+        type: 'IPA'
+    },
+    {
+        name: 'Orange Blossom Pilsner',
+        abv: 5.5,
+        label: 'https://s3.amazonaws.com/brewerydbapi/beer/Rczcb9/upload_9Nhxxl-large.png',
+        type: 'Pilsner'
+    },
+    {
+        name: 'Darkness',
+        abv: 4.2,
+        label: 'https://s3.amazonaws.com/brewerydbapi/beer/lnxbIV/upload_idNXFf-large.png',
+        type: 'Stout'
+    },
+    {
+        name: 'Belgian Wit',
+        abv: 5.4,
+        label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png',
+        type: 'Wheat'
+    },
+    {
+        name: 'Stolen Fruit',
+        abv: 4.6,
+        label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png',
+        type: 'Wheat'
+    },
+];
+
+const pizza = ['Deep Dish', 'Peperoni', 'Hawaiian', 'Meatzza', 'Spicy Mama', 'Margherita'];
+
+const cuts = ['Chuck', 'Brisket', 'Rib', 'Plate', 'Flank', 'Short Loin', 'Tenderloin', 'Sirloin', 'Round'];
+
+const inventory = {
+    shoes: 10,
+    socks: 6,
+    shirts: 10,
+    pants: 5,
+}
