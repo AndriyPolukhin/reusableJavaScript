929f53abc376270de7be753c1758d9d6bdc37ded
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-06-16 23:57:53 +0300

    webpack asset management

--- es6_19/13_es6module/es6webpack/src/index.js
+++ es6_19/13_es6module/es6webpack/src/index.js
@@ -1,14 +1,26 @@
-import _ from 'lodash';
+ // Lodash, now inported form the script
+ import _ from 'lodash';
+ import './style.scss';
+ import Icon from './icon.jpg';
+ import Data from './data.xml';
 
-function component() {
-  const element  = document.createElement('div');
+ function component() {
+   const element = document.createElement('div');
 
-  // Lodash, currently includes via a script, is required for this line to work
-  // Lodash, now inported form the script
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+   element.innerHTML = _.join(['Hello', 'there', 'webpack'], ' ');
+   element.classList.add('hello');
 
-document.body.append(component());
+   // Add the image ot our existing div.
+   var myIcon = new Image();
+   myIcon.src = Icon;
+
+   element.appendChild(myIcon);
+
+   console.log(Data);
+
+   return element;
+ }
+
+ document.body.append(component());
--- es6_19/13_es6module/es6webpack/webpack.config.js
+++ es6_19/13_es6module/es6webpack/webpack.config.js
@@ -1,10 +1,52 @@
 const path = require('path');
+const MiniCssExractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "production",
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
 };
