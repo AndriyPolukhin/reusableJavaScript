43127dcb43059b00bf0758f1d6dcb8d5e0c14f98
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-07-03 21:52:54 +0300

    react base

---
+++ react/catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>StorePicker Component Rendered</p>;
+  }
+}
+
+export default StorePicker;
+
---
+++ react/catch-of-the-day/src/index.js
@@ -0,0 +1,7 @@
+// This is the begining of the code
+
+import React from "react";
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+render( <StorePicker /> , document.querySelector('#main'));
