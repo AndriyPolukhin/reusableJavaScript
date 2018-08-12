819b385bc23ffe9fcea7b88baa1329dd6ee30728
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-08-13 00:02:11 +0300

    added Edit and Update Store Controllers

--- nodejs/node_overview/dang-thats-delicious2/controllers/storeController.js
+++ nodejs/node_overview/dang-thats-delicious2/controllers/storeController.js
@@ -18,6 +18,27 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
   // 1. Query the database for the list of all stores
   const stores = await Store.find();
-  console.log(stores);
+  // console.log(stores);
   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // res.json(store);
+  // 2. confirm they are the owner of the store
+  // @TODO
+  // 3. render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
+  // 2. redirect to the store and tell them it worked
+  res.redirect(`/stores/${store.id}/edit`);
 }
