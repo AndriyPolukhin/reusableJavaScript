f05c711fe54294cbe86817a1fb7c8f935c336993
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-08-14 18:46:26 +0300

    add the updates

--- nodejs/node_overview/dang-thats-delicious/models/Store.js
+++ nodejs/node_overview/dang-thats-delicious/models/Store.js
@@ -29,20 +29,26 @@ const storeSchema = new mongoose.Schema({
     }],
     address: {
       type: String,
-      required: 'Youm must supply an address!'
+      required: 'You must supply an address!'
     }
-  }
-
+  },
+  photo: String
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
   if (!this.isModified('name')) {
-    next();// skip it
-    return; //stop this function from running
+    next(); // skip it
+    return; // stop this function from running
   }
   this.slug = slug(this.name);
+  // find other stores that have a slug of wes, wes-1, wes-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
-  // Todo make more resilient so slugs are unique
+  // TODO make more resiliant so slugs are unique
 });
 
 module.exports = mongoose.model('Store', storeSchema);
--- nodejs/node_overview/dang-thats-delicious/public/javascripts/delicious-app.js
+++ nodejs/node_overview/dang-thats-delicious/public/javascripts/delicious-app.js
@@ -3,5 +3,6 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 
-
 autocomplete($('#address'), $('#lat'), $('#lng'));
+
+
