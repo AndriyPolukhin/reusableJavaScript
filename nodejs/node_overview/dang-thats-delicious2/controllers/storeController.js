ab1e6f173549a3f1ecf91381dc6c3762171d80b8
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-08-14 22:57:10 +0300

    added tag page

--- nodejs/node_overview/dang-thats-delicious2/controllers/storeController.js
+++ nodejs/node_overview/dang-thats-delicious2/controllers/storeController.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-
 const Store = mongoose.model('Store');
 const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
-
 
 const multerOptions = {
   storage: multer.memoryStorage(),
@@ -13,7 +11,7 @@ const multerOptions = {
     if (isPhoto) {
       next(null, true);
     } else {
-      next({ message: 'That filetype isn\'t allowed!', }, false);
+      next({ message: 'That filetype isn\'t allowed!' }, false);
     }
   }
 };
@@ -27,58 +25,65 @@ exports.addStore = (req, res) => {
 };
 
 exports.upload = multer(multerOptions).single('photo');
+
 exports.resize = async (req, res, next) => {
-  // 1. check if there is no new file to resize
+  // check if there is no new file to resize
   if (!req.file) {
     next(); // skip to the next middleware
     return;
   }
   const extension = req.file.mimetype.split('/')[1];
   req.body.photo = `${uuid.v4()}.${extension}`;
-  // 2.now i resize the photo
+  // now we resize
   const photo = await jimp.read(req.file.buffer);
   await photo.resize(800, jimp.AUTO);
   await photo.write(`./public/uploads/${req.body.photo}`);
-  // 3. once we have written the photo to our filesystem keep going!
+  // once we have written the photo to our filesystem, keep going!
   next();
 };
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-  res.redirect('/store/${store.slug}');
+  res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
-  // 1. Query the database for the list of all stores
+  // 1. Query the database for a list of all stores
   const stores = await Store.find();
-  // console.log(stores);
   res.render('stores', { title: 'Stores', stores });
 };
 
 exports.editStore = async (req, res) => {
   // 1. Find the store given the ID
   const store = await Store.findOne({ _id: req.params.id });
-  // res.json(store);
   // 2. confirm they are the owner of the store
-  // @TODO
-  // 3. render out the edit form so the user can update their store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
-  // 0. Set the location data to be a point
+  // set the location data to be a point
   req.body.location.type = 'Point';
-  // 1. find and update the store
-  const store = await Store.findOneAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    {
-      new: true, // return the new store instead of the old one
-      runValidators: true
-    }
-  ).exec();
-  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
-  // 2. redirect to the store and tell them it worked
-  res.redirect(`/stores/${store.id}/edit`);
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+  // Redriect them the store and tell them it worked
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render('store', { store, title: store.name });
+};
+
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', { tags, title: 'Tags', tag });
 };
--- nodejs/node_overview/dang-thats-delicious2/models/Store.js
+++ nodejs/node_overview/dang-thats-delicious2/models/Store.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
@@ -37,14 +38,30 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from runnning
   }
   this.slug = slug(this.name);
+  // find other stores that have a the same name
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+
   next();
   // @TODO make more resiliant so slugs are unique
 });
+
+// Creating a custom method on the StoreModel
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    { $unwind: "$tags" },
+    { $group: { _id: "$tags", count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]).cursor({}).exec().toArray();
+};
 
 module.exports = mongoose.model('Store', storeSchema);
