051c76b7e86f5040545ea4b94e33753f67e1edfa
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-08-16 00:20:39 +0300

    typeAhead functionality for the search

--- nodejs/node_overview/dang-thats-delicious2/public/javascripts/modules/typeAhead.js
+++ nodejs/node_overview/dang-thats-delicious2/public/javascripts/modules/typeAhead.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+import axios = from 'axios';
+import dompurify from 'dompurify';
 
 function searchResultsHTML(stores) {
   return stores.map(store => {
@@ -25,14 +26,17 @@ function typeAhead(search) {
     }
     // show the search results
     searchResults.style.display = 'block';
-    searchResults.innerHTML = '';
+
     axios
       .get(`/api/search?q=${this.value}`)
       .then(res => {
         if (res.data.length) {
           console.log('There is something to show!');
-          searchResults.innerHTML = searchResultsHTML(res.data);
+          searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));
+          return;
         }
+        // tell them nothing came back
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No result for ${this.value} found!</div>`);
       })
       .catch(err => {
         console.error(err);
@@ -47,6 +51,24 @@ function typeAhead(search) {
     }
     const activeClass = 'search__result--active';
     const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+    let next;
+    if (e.keyCode === 40 && current) {
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
   })
 };
 
