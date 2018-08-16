05b15a96b165d377b4d66598878894b74fa49127
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-08-17 01:01:33 +0300

    pagination

--- nodejs/node_overview/dang-thats-delicious2/routes/index.js
+++ nodejs/node_overview/dang-thats-delicious2/routes/index.js
@@ -8,6 +8,8 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/page/:page', catchErrors(storeController.getStores));
+
 router.get('/add',
   authController.isLoggedIn,
   storeController.addStore);
@@ -23,11 +25,7 @@ router.post('/add/:id',
   catchErrors(storeController.updateStore));
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-
-
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
-
-
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
